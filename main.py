33ce2ee8ab14cba8aea1a026c4c1dda617a0e7e0
jmkim00 <jmkim@superb-ai.com>
2024-07-02 22:33:16 +0900

    comment out showing map

--- main.py
+++ main.py
@@ -9,42 +9,45 @@ from Subway import Subway
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from streamlit_folium import st_folium 
+from streamlit_folium import st_folium
 import folium
 from folium import plugins
 import json
-from datetime import date
-from sklearn.preprocessing import StandardScaler
-import branca.colormap as cmp
+from branca.colormap import LinearColormap
+
 
 # Set Seoul 'dong' json file, map of Seoul
 
-# In the administrative division of South Korea, a "동" (dong) refers to a unit that is smaller than "읍" (eup) and "면" (myeon), 
-# typically used to describe smaller areas within cities or urban areas. 
-# A "dong" can encompass residential, commercial, or mixed-use areas and plays a significant role in the administrative structure of Korean cities, 
+# In the administrative division of South Korea, a "동" (dong) refers to a unit that is smaller than "읍" (eup) and "면" (myeon),
+# typically used to describe smaller areas within cities or urban areas.
+# A "dong" can encompass residential, commercial, or mixed-use areas and plays a significant role in the administrative structure of Korean cities,
 # providing various services for local residents and businesses.
 
-jpath = 'json/seoul_geo.json'
-seoul_geo = json.load(open(jpath, encoding = 'utf-8'))
+colormap = LinearColormap(
+    colors=["blue", "green", "yellow", "orange", "red"], vmin=0, vmax=100
+)
+
+jpath = "json/seoul_geo.json"
+seoul_geo = json.load(open(jpath, encoding="utf-8"))
 seoul_map = folium.Map(
-                    location = [37.5642135, 127.0016985], 
-                    tiles = 'cartoDB positron', 
-                    zoom_start = 10.5
-                    )
+    location=[37.5642135, 127.0016985], tiles="cartoDB positron", zoom_start=10.5
+)
 
 # Load subway station json file
-with open('json/station_coordinate.json', encoding='utf-8') as f:
+with open("json/station_coordinate.json", encoding="utf-8") as f:
     a = json.loads(f.read())
 js = pd.DataFrame(a)
-js = js.drop(columns = ['line', 'code'], axis=1)
-js = js.rename(columns = {'lat' : '위도', 'lng' : '경도', 'name' : '지하철역'})
+js = js.drop(columns=["line", "code"], axis=1)
+js = js.rename(columns={"lat": "위도", "lng": "경도", "name": "지하철역"})
 js = js.drop_duplicates().reset_index(drop=True)
 
 # Set title of Streamlit demo app
-st.set_page_config(page_title='Seoul Map', layout='centered')
+st.set_page_config(page_title="Seoul Map", layout="centered")
 st.title("Where to build futsal field?")
-st.caption("Made by 김윤재, 신지후, 정남주, 김종민 @ BDPV(Prof. Hyeonsil Moon) Kookmin Univ.")
-if st.button('새로고침'):
+st.caption(
+    "Made by 김윤재, 신지후, 정남주, 김종민 @ BDPV(Prof. Hyeonsil Moon) Kookmin Univ."
+)
+if st.button("새로고침"):
     st.experimental_rerun()
 
 # -------------------------------------- MAPPING ----------------------------------------------------
@@ -53,7 +56,9 @@ if st.button('새로고침'):
 LivingPopulationAcc(seoul_map, seoul_geo).livingPopulationAcc()
 
 # Preprocess & Mapping living population(average) data
-end_time, start_time, concatenated_df = LivingPopulationAvg(seoul_map, seoul_geo).livingPopulationAvg()
+end_time, start_time, concatenated_df = LivingPopulationAvg(
+    seoul_map, seoul_geo
+).livingPopulationAvg()
 
 # Preprocess & Mapping population density data
 PopulationDensity(seoul_map, seoul_geo).populationDensity()
@@ -67,14 +72,19 @@ end_time_sub, start_time_sub = Subway(seoul_map, js, end_time, start_time).subwa
 # Preprocess & Mapping Seoul public and private futsal field data
 FutsalField(seoul_map).futsalField()
 
-# folium load 
-folium.LayerControl().add_to(seoul_map)
-st_folium(seoul_map, width=725)
+# folium load
+# folium.LayerControl().add_to(seoul_map)
+# colormap.add_to(seoul_map)
+# st_folium(fig=seoul_map, width=725)
 
 # -------------------------------------- BARPLOT ----------------------------------------------------
 
 # population of boarding and disembarking of subway stations by subway line Barplot
-Barplot(end_time, start_time, end_time_sub, start_time_sub, concatenated_df).subwayBarplot()
+Barplot(
+    end_time, start_time, end_time_sub, start_time_sub, concatenated_df
+).subwayBarplot()
 
 # Living population of certain 'dong' Barplot
-Barplot(end_time, start_time, end_time_sub, start_time_sub, concatenated_df).livingPopulationBarplot()
+Barplot(
+    end_time, start_time, end_time_sub, start_time_sub, concatenated_df
+).livingPopulationBarplot()
